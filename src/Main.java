6d8f84398329bf9f1f976c4067b24d9864b0d050
audrey murua <audrey.murua1@gmail.com>
2022-07-09 00:12:49 -0500

    byte, short, long and width

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import com.sun.jdi.Value;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -16,6 +18,25 @@ public class Main {
         //underflow happened due to number being smaller than the minimum value
 
         int myMaxIntTest = 2_147_483_647;
+
+        byte myMinByteValue = Byte.MIN_VALUE;
+        byte myMaxByteValue = Byte.MAX_VALUE;
+        System.out.println("Byte Minimum Value = " + myMinByteValue);
+        System.out.println("Byte Maximum Value = " + myMaxByteValue);
+
+        short myMinShortValue = Short.MIN_VALUE;
+        short myMaxShortValue = Short.MAX_VALUE;
+        System.out.println("Short Minimum Value = " + myMinShortValue);
+        System.out.println("Short Maximum Value = " + myMaxShortValue);
+
+        long myLongValue = 100L;
+        long myMinLongValue = Long.MIN_VALUE;
+        long myMaxLongValue = Long.MAX_VALUE;
+        System.out.println("Long Minimum Value = " + myMinLongValue);
+        System.out.println("Long Maximum Value = " + myMaxLongValue);
+
+        long bigLongLiteralValue = 2_147_483_647_234L;
+        System.out.println(bigLongLiteralValue);
 
 
     }
