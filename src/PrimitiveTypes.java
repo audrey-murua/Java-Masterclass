07c8d45d14b52405bc126679adcf7ab68bf5c832
audrey murua <audrey.murua1@gmail.com>
2022-07-15 01:11:28 -0500

    char and boolean data types

--- src/PrimitiveTypes.java
+++ src/PrimitiveTypes.java
@@ -92,6 +92,21 @@ public class PrimitiveTypes {
         System.out.println(pi);
 
 
+        // Char and Boolean Primitive Data Types
+
+        char myChar = 'D';
+        char myUnicodeChar = '\u0044';
+        System.out.println(myChar);
+        System.out.println(myUnicodeChar);
+
+        char myCopyrightChar = '\u00A9';
+        System.out.println(myCopyrightChar);
+
+        boolean myTrueBooleanValue = true;
+        boolean myFalseBooleanValue = false;
+        boolean isCustomerOverTwentyOne = true;
+
+
 
 
     }
